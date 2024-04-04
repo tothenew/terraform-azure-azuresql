eff2448fb5676284f02838b29cab7bdd1fe5e0ca
Ankit Kaushik <klatte09>
2024-04-04 11:50:43 +0530

    chagnes for vaildation

---
+++ _locals.tf
@@ -0,0 +1,68 @@
+locals {
+  vcore_tiers                 = ["GeneralPurpose", "BusinessCritical"]
+  elastic_pool_vcore_family   = try(var.elastic_pool_sku.family, "Gen5")
+  elastic_pool_vcore_sku_name = var.elastic_pool_sku != null ? format("%s_%s", var.elastic_pool_sku.tier == "GeneralPurpose" ? "GP" : "BC", local.elastic_pool_vcore_family) : null
+  elastic_pool_dtu_sku_name   = var.elastic_pool_sku != null ? format("%sPool", var.elastic_pool_sku.tier) : null
+  elastic_pool_sku = var.elastic_pool_sku != null ? {
+    name     = contains(local.vcore_tiers, var.elastic_pool_sku.tier) ? local.elastic_pool_vcore_sku_name : local.elastic_pool_dtu_sku_name
+    capacity = var.elastic_pool_sku.capacity
+    tier     = var.elastic_pool_sku.tier
+    family   = contains(local.vcore_tiers, var.elastic_pool_sku.tier) ? local.elastic_pool_vcore_family : null
+  } : null
+
+  allowed_subnets = [
+    for id in var.allowed_subnets_ids : {
+      name      = split("/", id)[10]
+      subnet_id = id
+    }
+  ]
+
+  databases_users = var.create_databases_users ? [
+    for db in var.databases : {
+      username = format("%s_user", replace(db.name, "-", "_"))
+      database = db.name
+      roles    = ["db_owner"]
+    }
+  ] : []
+
+  standard_allowed_create_mode = {
+    "a" = "Default"
+    "b" = "Copy"
+    "c" = "Secondary"
+    "d" = "PointInTimeRestore"
+    "e" = "Restore"
+    "f" = "Recovery"
+    "g" = "RestoreExternalBackup"
+    "h" = "RestoreExternalBackup"
+    "i" = "RestoreLongTermRetentionBackup"
+    "j" = "OnlineSecondary"
+  }
+
+  datawarehouse_allowed_create_mode = {
+    "a" = "Default"
+    "b" = "PointInTimeRestore"
+    "c" = "Restore"
+    "d" = "Recovery"
+    "e" = "RestoreExternalBackup"
+    "f" = "RestoreExternalBackup"
+    "g" = "OnlineSecondary"
+  }
+}
+
+
+locals {
+  # Naming locals/constants
+  name_prefix = lower(var.name_prefix)
+  name_suffix = lower(var.name_suffix)
+
+  server_name       = coalesce(var.server_custom_name, data.azurecaf_name.sql.result)
+  elastic_pool_name = coalesce(var.elastic_pool_custom_name, data.azurecaf_name.sql_pool.result)
+}
+
+
+locals {
+  default_tags = var.default_tags_enabled ? {
+    env   = var.environment
+    stack = var.stack
+  } : {}
+}
